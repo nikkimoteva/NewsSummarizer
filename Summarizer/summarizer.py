04052b4fc366ee54e9adbc2b73e8ca228fcd5b26
nikkimoteva <37060980+nikkimoteva@users.noreply.github.com>
2020-06-22 04:25:45 -0700

    initialize summarizer

---
+++ Summarizer/summarizer.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# summarize the text in 300 words or 5 sentences maximum.
+
+# make sure that you uncode the abbriviation, so the analyzer won't get confused:
+# eg. sentence ending when reached Mr. ; won't = would not
+
+# compare 3 stories, combine them into an unbiased text summary
+# skip any other occurance of that story when bias <0.2 - use Azure for bias analysis
+
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
