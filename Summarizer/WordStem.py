f39b8d21dde754b80367ea6aabe7537be5e15b83
Nikki <nikkimoteva@gmail.com>
2020-07-29 23:36:47 -0700

    added functionality for summarizing the news using the stem root of the words using nltk snowball

---
+++ Summarizer/WordStem.py
@@ -0,0 +1,9 @@
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.isri import ISRIStemmer
+from nltk.stem.rslp import RSLPStemmer
+
+def stemmer(wordList, language):
+    for word in wordList:
+        wordStemmer = SnowballStemmer(language, ignore_stopwords=True)
+        word = wordStemmer.stem(word)
+    return wordList
