e3a67428ec1864e03b97d7021dad8d2d118ac583
Nikki <nikkimoteva@gmail.com>
2020-07-29 23:25:16 -0700

    added OOP support

--- Summarizer/mongodbDAO.py
+++ Summarizer/mongodbDAO.py
@@ -2,13 +2,27 @@ import pymongo as pm
 
 class Mongo:
     def __init__(self):
-        client = pm.MongoClient("mongodb+srv://admin:admin@newssummary.xjugy.azure.mongodb.net/NewsSumm?retryWrites=true&w=majority")
-        db = client['News']
-        print(db)
-        collection = db['NewsSumm']
-        print(collection)
+        getClient = self.get_client()
+        self.client = pm.MongoClient(getClient)
+        getDB = self.get_db()
+        self.db = self.client[getDB]
+        getCollection = self.get_collection()
+        self.collection = self.db[getCollection]
 
-    def inserInDB(self, collection, title, url, who, summary):
+    def get_client(self):
+        return "mongodb+srv://admin:admin@newssummary.xjugy.azure.mongodb.net/NewsSumm?retryWrites=true&w=majority"
+    
+    def get_db(self):
+        return "News"
+
+    def get_collection(self):
+        return 'NewsSumm'
+
+    def getSession(self):
+        return self.collection
+
+    def inserInDB(self, title, url, who, summary):
+        collection = self.getSession()
         new_insert = {"title": title,
                     "url": url,
                     "who": who,
@@ -17,14 +31,19 @@ class Mongo:
         print(check.inserted_id)
         
     def findAllDB(self, collection, country):
+        collection = self.getSession()
         query = {"country": country}
         results = collection.find(query)
         #returns a list of the results
         return results
 
-    def deleteAllDB(self, collection):
+    def deleteAllDB(self):
+        collection = self.getSession()
         #delete all db right before update
         collection.delete_many({})
+
+    def closeConnection(self):
+        self.client.close()
     
 
     
