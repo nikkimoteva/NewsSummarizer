95c9c8b7208e1db008b7ad6fa31823430c08afd5
Nikki <nikkimoteva@gmail.com>
2020-07-30 22:25:35 -0700

    Clean up, added new var to the object

--- Summarizer/news.py
+++ Summarizer/news.py
@@ -1,36 +1,50 @@
 from newsapi import NewsApiClient
 
-class newsArticle:
-    def __init__(self, news_api, country):
+class NewsArticle:
+    def __init__(self, news_api, country, tone = 0):
         self.title = ""
         self.country = ""
         self.description = ""
         self.url = ""
-        self.setTitle(news_api['title'])
-        self.setCountry(country)
-        self.setDesc(news_api['content'])
-        self.setURL(news_api['url'])
+        self.tone = 0
+        self.lang = ""
+        self.SetTitle(news_api['title'])
+        self.SetCountry(country)
+        self.SetDescription(news_api['body'])
+        self.SetURL(news_api['url'])
+        self.SetTone(tone) 
+        self.SetLang(news_api['lang'][:2])
 
-    def get_body(self, news_api):
+    def GetBody(self, news_api):
         return news_api.get_top_headlines()['articles']
     
-    def setTitle(self, title):
+    def SetTitle(self, title):
         self.title = title
-    def get_title(self):
+    def GetTitle(self):
         return self.title
     
-    def setCountry(self, country):
+    def SetCountry(self, country):
         self.country = country
-    def get_country(self):
+    def GetCountry(self):
         return self.country
     
-    def setDesc(self, description):
+    def SetDescription(self, description):
         self.description = description
-    def get_description(self):
+    def GetDescription(self):
         return self.description
     
-    def setURL(self, url):
+    def SetURL(self, url):
         self.url = url
-    def get_url(self):
+    def GetUrl(self):
         return self.url
+
+    def SetTone(self, tone):
+        self.tone = tone
+    def GetTone(self):
+        return self.tone
+
+    def SetLang(self, lang):
+        self.lang = lang
+    def GetLang(self):
+        return self.lang
 
