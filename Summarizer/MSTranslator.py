af19419c2809222f38d9a6054260cdfdb45f6a2e
Nikki <nikkimoteva@gmail.com>
2020-07-30 22:21:00 -0700

    clean up, added max char length per article

--- Summarizer/MSTranslator.py
+++ Summarizer/MSTranslator.py
@@ -1,26 +1,29 @@
 import requests, uuid, json
 
-def translate(lang_from, lang_to, text):
-    key = '85c15359e016483483c6d4501ecea3ed'
+def Translate(langFrom, langTo, text):
+    apiKey = '85c15359e016483483c6d4501ecea3ed'
     endpoint = 'https://api.cognitive.microsofttranslator.com/'
 
     path = '/translate?api-version=3.0'
-    params = "&from=" + lang_from +"&to=" + lang_to
-    constructed_url = endpoint + path + params
+    params = "&from=" + langFrom +"&to=" + langTo
+    constructedUrl = endpoint + path + params
 
     headers = {
-        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Key': apiKey,
         'Content-type': 'application/json',
         'X-ClientTraceId': str(uuid.uuid4())
     }
+
+    if len(text) > 5000:
+        text = text[:5000]
 
     body = [{
         'text' : text
 
     }]
-    request = requests.post(constructed_url, headers=headers, json=body)
-    response = request.json()[0]
-    if response:
-        response = request.json()[0]["translations"]
-        return response[0]['text']
+    request = requests.post(constructedUrl, headers=headers, json=body)
+    if request.json():
+        if request.json()[0]:
+            response = request.json()[0]["translations"]
+            return response[0]['text']
     else: return "dang"
